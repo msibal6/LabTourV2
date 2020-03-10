aa31b05948b57b2c422d0d1ff22b743e4c528319
Kelly Chan <kellychan@campus-110-120.ucdavis.edu>
2020-03-10 13:25:22 -0700

    updated commit (nothing new on your part)

--- Assets/Scripts/MySceneManager.cs
+++ Assets/Scripts/MySceneManager.cs
@@ -43,7 +43,5 @@ public class MySceneManager : MonoBehaviour
     {
         SceneManager.LoadScene(name);
 
-    }
-
-   
+    }  
 }
---
+++ Assets/Scripts/TakeImage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakeImage : MonoBehaviour
+{
+	// public static class TakeImage;
+
+	public static Button PictureTaken;
+
+    void Start()
+    {
+        Button btn = PictureTaken.GetComponent<Button>();
+        btn.onClick.AddListener(TakePic);
+    }
+
+
+    public void TakePic()
+    {
+
+        Debug.Log("Hello.");
+
+    }
+}
