ca794297933944e9c42a36c6f307f948d12f3db9
Mitchell Sibal <mitchell.sibal@gmail.com>
2020-03-12 16:05:10 -0700

    Fixed duplicate outline for the looking part of the Microscope

--- Assets/Scripts/DuplicateOutline.cs
+++ Assets/Scripts/DuplicateOutline.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class DuplicateOutline : MonoBehaviour
 {
 
+    public Material replaceMaterial;
+
+    private Renderer rend;
+    private Material[] myMaterials;
+    private Material[] tempMaterials;
+
     public InteractionController controller;
-    public GameObject  outliner;
+    public GameObject other;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = gameObject.GetComponentsInChildren<MeshRenderer>()[1];
+        rend.enabled = true;
+        tempMaterials = rend.sharedMaterials;
+        myMaterials = rend.sharedMaterials;
+
+        for (int i = 0; i < myMaterials.Length; i++) myMaterials[i] = replaceMaterial;
     }
 
     // Update is called once per frame
@@ -19,12 +30,29 @@ public class DuplicateOutline : MonoBehaviour
         RaycastHit hit = controller.GetRaycastHit();
         if (hit.collider != null && hit.collider.gameObject == gameObject)
         {
-            print("sending outline");
-            outliner.SendMessage("EnableOutline");
+            EnableOutline();
+        }
+        else if (hit.collider != null && hit.collider.gameObject == other)
+        {
+            EnableOutline();
         }
         else
         {
-            outliner.SendMessage("DisableOutline");
+            DisableOutline();
         }
     }
+
+    public void DisableOutline()
+    {
+        rend.sharedMaterials = tempMaterials;
+
+
+    }
+
+    public void EnableOutline()
+    {
+        rend.sharedMaterials = myMaterials; 
+    }
+
+
 }
